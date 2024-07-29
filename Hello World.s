269a4facd3c3e3855e6c057d99c9994524efac5a
Miguel <76119303+xMigma@users.noreply.github.com>
2024-07-29 16:17:24 +0200

    Update Hello World.s

--- Hello World.s
+++ Hello World.s
@@ -1,8 +1,7 @@
 .data
 	hello: .string "Hello world"
 .text 
-	main:
-      # Decir que en ensamblador no existen las variables
+main:
       li a7 4 # llamar a print
       la a0 hello # pasar el argumento
       ecall # hacer la llamada
